86452d8cd9df7ce4dd76cf32fd32c74ba379b3a9
Manasa-dev503 <198a1a0503manasa@gmail.com>
2024-08-05 17:34:15 +0530

    cdk

--- lib/my-cdk-project-stack.ts
+++ lib/my-cdk-project-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3'
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,11 @@ export class MyCdkProjectStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MyCdkProjectQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+  
+    new s3.Bucket(this,'MyBucket',{
+      versioned : true
+
+    })
   }
 }
